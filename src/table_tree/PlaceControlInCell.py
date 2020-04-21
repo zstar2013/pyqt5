a3c76cc90f2666efbe13b83e1569c215ca54d46c
znostar <zx123star@163.com>
2020-04-21 17:43:39 +0800

    123

---
+++ src/table_tree/PlaceControlInCell.py
@@ -0,0 +1,63 @@
+'''
+
+拓展的列表控件（TableWidget）
+
+QTableView
+
+每一个cell(单元格)是一个TableWidgetItem
+'''
+
+import sys,math
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import Qt, QStringListModel
+
+
+class PlaceControlInCell(QWidget):
+    def __init__(self):
+        super(PlaceControlInCell, self).__init__()
+        self.initUI()
+
+    def initUI(self):
+        layout=QHBoxLayout()
+        tablewidget=QTableWidget()
+        tablewidget.setRowCount(4)
+        tablewidget.setColumnCount(3)
+
+        layout.addWidget(tablewidget)
+
+        tablewidget.setHorizontalHeaderLabels(['姓名','年龄','籍贯'])
+        nameItem=QTableWidgetItem('小明')
+        tablewidget.setItem(0,0,nameItem)
+        ageItem = QTableWidgetItem('24')
+        tablewidget.setItem(0, 1, ageItem)
+        jgItem=QTableWidgetItem('北京')
+        tablewidget.setItem(0,2,jgItem)
+
+        #禁止编辑
+        tablewidget.setEditTriggers(QAbstractItemView.NoEditTriggers)
+
+        #整行选择
+        tablewidget.setSelectionBehavior(QAbstractItemView.SelectRows)
+
+        #调整行和列
+        tablewidget.resizeColumnsToContents()
+        tablewidget.resizeRowToContents()
+
+        #tablewidget.horizontalHeader().setVisible(False)
+        #tablewidget.verticalHeader().setVisible(False)
+
+        tablewidget.setVerticalHeaderLabels(["a","b"])
+
+        self.setLayout(layout)
+
+        self.resize(430, 230)
+        self.setWindowTitle("TableWidgetDemo例子")
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    main = PlaceControlInCell()
+    main.show()
+
+    sys.exit(app.exec_())
--- src/table_tree/TableWidgetDemo.py
+++ src/table_tree/TableWidgetDemo.py
@@ -13,7 +13,7 @@ from PyQt5.QtWidgets import *
 from PyQt5.QtCore import Qt, QStringListModel
 
 
-class TableWidgetDemo(QMainWindow):
+class TableWidgetDemo(QWidget):
     def __init__(self):
         super(TableWidgetDemo, self).__init__()
         self.initUI()
@@ -29,25 +29,30 @@ class TableWidgetDemo(QMainWindow):
         tablewidget.setHorizontalHeaderLabels(['姓名','年龄','籍贯'])
         nameItem=QTableWidgetItem('小明')
         tablewidget.setItem(0,0,nameItem)
+        ageItem = QTableWidgetItem('24')
+        tablewidget.setItem(0, 1, ageItem)
+        jgItem=QTableWidgetItem('北京')
+        tablewidget.setItem(0,2,jgItem)
 
+        #禁止编辑
+        tablewidget.setEditTriggers(QAbstractItemView.NoEditTriggers)
+
+        #整行选择
+        tablewidget.setSelectionBehavior(QAbstractItemView.SelectRows)
+
+        #调整行和列
+        tablewidget.resizeColumnsToContents()
+        tablewidget.resizeRowToContents()
+
+        #tablewidget.horizontalHeader().setVisible(False)
+        #tablewidget.verticalHeader().setVisible(False)
+
+        tablewidget.setVerticalHeaderLabels(["a","b"])
+
+        self.setLayout(layout)
 
         self.resize(430, 230)
         self.setWindowTitle("TableWidgetDemo例子")
-        self.setCentralWidget(self.listwdiget)
-
-
-
-    def clicked(self,Index):
-        QMessageBox.information(self,"QlistView","您选择了："+self.listwdiget.item(self.listwdiget.row(Index)).text())
-
-
-
-
-
-    def process(self,a):
-        print(self.sender().text())
-
-
 
 
 if __name__ == '__main__':
